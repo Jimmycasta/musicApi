80fd4bfe4782ed183adf733b34f6813539746045
jimmycasta <jimmycasta@gmail.com>
2023-05-31 16:27:25 -0500

    Se crea entity, repositorio y primer controlador

--- src/main/java/com/jimmycasta/music/controller/AlbumController.java
+++ src/main/java/com/jimmycasta/music/controller/AlbumController.java
@@ -3,9 +3,7 @@ package com.jimmycasta.music.controller;
 import com.jimmycasta.music.entity.Album;
 import com.jimmycasta.music.service.IAlbumService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,7 +15,24 @@ public class AlbumController {
     private IAlbumService albumService;
 
     @GetMapping("/album")
-    public List<Album> buscarTodos(){
+    public List<Album> buscarTodos() {
         return albumService.buscarTodos();
+    }
+
+    @PostMapping("/album")
+    public Album guardar(@RequestBody Album album) {
+        albumService.guardar(album);
+        return album;
+    }
+
+    @PutMapping("/album")
+    public Album actualizar(@RequestBody Album album) {
+        albumService.guardar(album);
+        return album;
+    }
+    @DeleteMapping("/album/{id}")
+    public String eliminar(@PathVariable("id") int id){
+        albumService.eliminar(id);
+        return "Registro eliminado";
     }
 }
--- src/main/java/com/jimmycasta/music/entity/Album.java
+++ src/main/java/com/jimmycasta/music/entity/Album.java
@@ -3,6 +3,8 @@ package com.jimmycasta.music.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Entity
 @Table(name = "albums")
 @Data
@@ -11,4 +13,9 @@ public class Album {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String titulo;
+    private LocalDate lanzado;
+    private double precio;
+    private String genero;
+
+
 }
